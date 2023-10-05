482913165972744ad8847b12f0975ae21d56a0d7
khaing17 <aungthetkhaing017@gmail.com>
2023-10-05 07:37:57 +0630

    first-commit

---
+++ src/PostContext.js
@@ -0,0 +1,57 @@
+import React, { useContext, useMemo } from "react";
+import { createContext, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+const PostContext = createContext();
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onAddPosts: handleAddPost,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+    };
+  }, [searchedPosts, searchQuery]);
+
+  return <PostContext.Provider value={value}>{children}</PostContext.Provider>;
+}
+
+function usePosts() {
+  const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error("PostContext was used outside of the PostProvider.");
+  return context;
+}
+
+export { PostProvider, usePosts };
